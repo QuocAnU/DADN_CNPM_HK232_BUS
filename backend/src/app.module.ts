2e756bd47bf733feba75b5a1b8d6f4a094b3d8cb
QuocAnU <an.lequoc2103@hcmut.edu.vn>
2024-05-24 12:54:44 +0700

    login

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,18 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule } from '@nestjs/mongoose'
 import { BusRoutesModule } from './bus-routes/bus-routes.module';
 import { BusModule } from './bus/bus.module';
 import { BusStopsModule } from './bus-stops/bus-stops.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
   imports: [
     MongooseModule.forRoot("mongodb+srv://anlequoc2103:gN33oQ4aScZewTLf@bus.jk6gdpu.mongodb.net/?retryWrites=true&w=majority&appName=BUS"),
+    ConfigModule.forRoot({ isGlobal: true }),
     BusRoutesModule,
     BusModule,
     BusStopsModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ backend/src/auth/dto/registerDto.ts
@@ -0,0 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class RegisterDto {
+  @ApiProperty({
+    type: String,
+    description: 'The email address for the new user',
+    example: 'jane@example.com',
+  })
+  @IsNotEmpty()
+  @IsString()
+  email: string;
+
+  @ApiProperty({
+    type: String,
+    description: 'The password for the new user',
+    example: 'mysecretpassword',
+  })
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+
+  @ApiProperty()
+  type: string;
+  @IsNotEmpty()
+  @IsString()
+
+  
+  fullname: string;
+}
